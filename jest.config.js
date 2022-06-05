8c320ed5a81eb0d97b5871aee842c772859f0cb0
Eliel Simoncelo de Sousa <sousaeliel@gmail.com>
2022-06-05 18:10:13 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -97,7 +97,10 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: 'ts-jest',
+  preset: [
+    'ts-jest',
+    '@shelf/jest-mongodb'
+  ],
 
   // Run tests from one or more projects
   // projects: undefined,

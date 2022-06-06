82cf0b898c7b18de24d038cbb777b09f314cda78
Eliel Simoncelo de Sousa <sousaeliel@gmail.com>
2022-06-05 22:44:14 -0300

    refactor: change result map

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,7 +7,8 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
+    const id = result.insertedId.toString()
 
-    return Object.assign({}, accountData, { id: result.insertedId.toString() })
+    return { id, ...accountData }
   }
 }
